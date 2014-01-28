759d74040b82a39344b943ca97da4fe4adee6e8a
Alexis Hope <bambishope@gmail.com>
2014-01-28 12:36:58 +1100

    set blank main template

--- code/JaxifyController.php
+++ code/JaxifyController.php
@@ -5,8 +5,9 @@ class JaxifyController extends Extension {
     public static $allowed_actions = array('ajax');
 
     public function ajax() {
-        $tpl = get_class( $this->owner->dataRecord );
-        return $this->owner->renderWith( $tpl );
+		$view = $this->owner->getViewer( $this->owner->getAction() );
+		$view->setTemplateFile( 'main', getcwd() . '/../ss-jaxify/templates/jaxifymain.ss' );
+		return $view->process( $this->owner );
     }
     
 }
